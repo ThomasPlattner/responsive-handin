9ac8b65a5374920c8c1eead828d0a0cda280ca5d
Thomas Plattner <tompl@Tommys-MacBook-Air.local>
2023-03-25 21:00:51 +0100

    Refactoring: Model & unit tests
    further cleanup of duplicate files from merge conflict during last conflict; adding first model for article list; building foundation for unit tests

--- app/app.py
+++ app/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, redirect, url_for, render_template
 from .articles.routes import article_list
 from . import articles, general_pages
-# from app.extensions.database import db
+from app.extensions.database import db, migrate
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config')
 
-    # register_extensions(app)
+    register_extensions(app)
     register_blueprints(app)
 
     return app
@@ -18,5 +18,6 @@ def register_blueprints(app: Flask):
     app.register_blueprint(general_pages.routes.blueprint)
 
 # Database
-# def register_extensions(app: Flask):
-#     db.init_app(app)
+def register_extensions(app: Flask):
+    db.init_app(app)
+    migrate.init_app(app, db, compare_type=True)
--- app/articles/models.py
+++ app/articles/models.py
@@ -1,9 +1,18 @@
-# class Article():
-#     id = ''
-#     title = ''
-#     icon = ''
-#     text = ''
-#     date = ''
-#     image_url = ''
-#     image_name = ''
-#     image_alt = ''
+from app.extensions.database import db
+from datetime import datetime
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=True)
+    icon = db.Column(db.String)
+    text = db.Column(db.Text)
+    date = db.Column(db.Date)
+    image_url = db.Column(db.String)
+    image_name = db.Column(db.String)
+    image_alt = db.Column(db.String)
+
+# class Article_link(db.Model):
+#     id = id.Column(db.Integer, primary_key=True)
+#     image_url = db.Column(db.String)
+#     image_name = db.Column(db.String)
+#     image_alt = db.Column(db.String)

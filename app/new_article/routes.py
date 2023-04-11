586bdf34dfb1e80a0f5c7c64506cd7ec070a5cec
Thomas Plattner <tompl@Tommys-MacBook-Air.local>
2023-04-11 16:35:17 +0200

    html forms
    fixed bug with html form for article for foreign keys

--- app/new_article/routes.py
+++ app/new_article/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from app.articles.models import Category, Article, User
+from app.articles.models import Category, Article, User, ArticleCategory
 
 blueprint = Blueprint('new_article', __name__)
 
@@ -10,11 +10,44 @@ def get_article():
 
 @blueprint.post('/new')
 def post_article():
+    # user_name = request.form['user']
+    # user = User.query.filter(User.id == user_name).first()
+
     # create an article
     article = Article(
-        icon = request.form['icon']
+        title = request.form['title'],
+        icon = request.form['icon'],
+        text = request.form['text'],
+        user_id = request.form['user'],
     )
     article.save()
 
     categories = Category.query.all()
-    return render_template('new_article/new.html', categories=categories)
+    users = User.query.all()
+    return render_template('new_article/new.html', categories=categories, users=users)
+
+@blueprint.get('/new-category')
+def get_new_article():
+    return render_template('new_article/new_category.html')
+
+@blueprint.post('/new-category')
+def post_new_article():
+    # create a new category
+    category = Category(
+        categories = request.form['new_categories']
+    )
+    category.save()
+    return render_template('new_article/new_category.html')
+
+@blueprint.get('/new-user')
+def get_new_user():
+    return render_template('new_article/new_user.html')
+
+@blueprint.post('/new-user')
+def post_new_user():
+    # create a new user
+    user = User(
+        name = request.form['new_user'],
+    )
+    user.save()
+    return render_template('new_article/new_user.html')

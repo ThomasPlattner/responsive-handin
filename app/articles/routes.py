418234c6d88fb11511b7fb9e37eec0c6c6d5b0ba
Thomas Plattner <tompl@Tommys-MacBook-Air.local>
2023-04-14 01:24:52 +0200

    articles rendered in template using text from database
    articles were previously in their own html file. Changed the rout to now load a template that renders the individual article

--- app/articles/routes.py
+++ app/articles/routes.py
@@ -3,14 +3,26 @@ from .models import Article
 
 blueprint = Blueprint('articles', __name__)
 
+# @blueprint.route('/blog/<slug>')
+# def blog(slug):
+#     blogs = Article.query.all()
+#     for article in blogs:
+#         if slug.title() == article.title:
+#             url = "articles/" + slug.lower() + ".html"
+#             return render_template(url)
+#             break
+#     else:
+#         message = "Sorry, we couldn't find the article about " + slug + " :( "
+#         return render_template('/base.html', message=message)
+    
 @blueprint.route('/blog/<slug>')
 def blog(slug):
     blogs = Article.query.all()
     for article in blogs:
         if slug.title() == article.title:
-            url = "articles/" + slug.lower() + ".html"
-            return render_template(url)
+            text = article.text
+            return render_template('articles/article_template.html', text=text)
             break
-    else:
-        message = "Sorry, we couldn't find the article about " + slug + " :( "
-        return render_template('/base.html', message=message) 
+        else:
+            message = "Sorry, we couldn't find the article about " + slug + " :( "
+            return render_template('/base.html', message=message)
